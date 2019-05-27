5aedf64842e96c05ce14ac419abff9d7f93d939c
yjsong <yjsong@yjsongdeMBP.lan>
2019-05-27 22:48:56 +0800

    leetcode1

---
+++ LeetCode/Leetcode001_twoSum/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Leetcode1_twoSum
+//
+//  Created by yjsong on 2019/5/27.
+//  Copyright © 2019 宋永建. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
