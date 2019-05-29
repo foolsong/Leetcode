c9a5509a13389f626128a337b2b832d5e6b07c5e
songyongjian <songyongjian@meituan.com>
2019-05-29 14:06:47 +0800

    :)

---
+++ LeetCode/Leetcode001_twoSum/hashMethod.h
@@ -0,0 +1,14 @@
+//
+//  hashMethod.h
+//  Leetcode001_twoSum
+//
+//  Created by 宋永建 on 2019/5/29.
+//  Copyright © 2019 宋永建. All rights reserved.
+//
+
+#ifndef hashMethod_h
+#define hashMethod_h
+
+#include <stdio.h>
+
+#endif /* hashMethod_h */
--- LeetCode/Leetcode001_twoSum/main.m
+++ LeetCode/Leetcode001_twoSum/main.m
@@ -8,9 +8,48 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+struct node {
+    int val;
+    int index;
+};
+int comp(const void* a, const void* b) {
+    return (((struct node*)a)->val > ((struct node*)b)->val ? 1 : -1);
+}
+int* twoSum(int* nums, int numsSize, int target) {
+    int i = 0;
+    int n = numsSize;
+    int begin = 0, end = n - 1;
+    int* result = NULL;
+    struct node* nodes = (struct node*)malloc(n * sizeof(struct node));
+    // init nodes
+    for(i = 0; i < n; i++) {
+        nodes[i].val = nums[i];
+        nodes[i].index = i;
+    }
+    qsort(nodes, n, sizeof(struct node), comp);
+    while(begin < end) {
+        if(nodes[begin].val + nodes[end].val == target) {
+            result = (int*)malloc(sizeof(int) * 2);
+            result[0] = nodes[begin].index;
+            result[1] = nodes[end].index;
+            free(nodes);
+            return result;
+        }
+        else if(nodes[begin].val + nodes[end].val > target)
+            end--;
+        else
+            begin++;
+    }
+    free(nodes);
+    return result;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+        
         NSLog(@"Hello, World!");
     }
     return 0;
