c6373c96eacffc13aef17056924dc02414cf1c5b
songyongjian <songyongjian@meituan.com>
2019-06-07 16:03:03 +0800

    :)

---
+++ LeetCode/Leetcode005_LongestPalindromicSubstring/main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  Leetcode005_LongestPalindromicSubstring
+//
+//  Created by 宋永建 on 2019/6/7.
+//  Copyright © 2019 宋永建. All rights reserved.
+//
+
+
+/*
+ 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ 
+ 示例 1：
+ 输入: "babad"
+ 输出: "bab"
+ 注意: "aba" 也是一个有效答案。
+ 
+ 示例 2：
+ 输入: "cbbd"
+ 输出: "bb"
+ 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ LeetCode/Leetcode009_PalindromeNumber/main.c
@@ -0,0 +1,39 @@
+//
+//  main.c
+//  Leetcode009_PalindromeNumber
+//
+//  Created by 宋永建 on 2019/6/7.
+//  Copyright © 2019 宋永建. All rights reserved.
+//
+
+/*
+ 
+ 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ 
+ 示例 1:
+ 输入: 121
+ 输出: true
+ 
+ 示例 2:
+ 输入: -121
+ 输出: false
+ 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ 
+ 示例 3:
+ 输入: 10
+ 输出: false
+ 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ 进阶:
+ 
+ 你能不将整数转为字符串来解决这个问题吗？
+ 
+ 
+ */
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
